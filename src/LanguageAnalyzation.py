d80bdaae958b63e385d67d4afe923781d9579a1f
Rafael <rr@cinequant.com>
2012-04-12 14:56:49 +0200

    fonction frequency_of refaite

--- src/LanguageAnalyzation.py
+++ src/LanguageAnalyzation.py
@@ -3,24 +3,25 @@ Created on Apr 10, 2012
 
 @author: rafael
 '''
+from __future__ import division
 import MySQLdb
 import re
 import operator
 
-def countWords(myTable):
-    allWords=[]
+
+
+def count_words(myTable):
     #replace vector for dict(hashmap for python)
-    dictionary=dict(one=1, two=2)
+    dictionary={}
     for comment in myTable:
         p=re.findall('\w+|[,;.!?]', comment)
         for word in p:
-            if word not in dictionary: #
-                dictionary.insert(word,1)#
+            if word not in dictionary: 
+                dictionary.insert(word,1)
             else: 
                 dictionary[word]=dictionary.get(word)+1
                 print dictionary[word]
-               # dictionary.get(k)=dictionary.get(word)+1
-        #do a sort
+               
         
         
 def test():
@@ -29,21 +30,21 @@ def test():
     p=p+p
     print p
     myTable=[s,s,s]
-    countWords(myTable)
+    count_words(myTable)
     
 
-def getLabeledComments(stars):
+def get_labeled_comments(stars):
     db=MySQLdb.connect("cinequant.com","rafael","nqq2612","rafael",use_unicode=True,charset="utf8")
     cursor=db.cursor()
     query="""SELECT `review` FROM `rafael`.`cinefrance_moviereviews` WHERE `rating`=%s"""%stars
     cursor.execute(query)
-    dictionary={}
+    dictionary={"total of comments":cursor.rowcount}
     for t in cursor.fetchall():
         p=re.findall('[^\s,;.!?]+|[,;.!?]', t[0])
         for word in p:
             if word in dictionary: #
                 dictionary[word]=dictionary.get(word)+1
-                print dictionary[word]
+                #print dictionary[word]
             else: 
                 dictionary[word]=1
                 
@@ -52,11 +53,19 @@ def getLabeledComments(stars):
             print duple'''
     return dictionary        
     
-        #n=allWords.count(value)
-        #if n>10:
-         #   print [value, n]
-    
-    
-    
-    
 
+def frequency_of(cle):
+    print 'How many times did the word '+cle+' appears in average in comments with:'
+    for i in (5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, 0.5):
+        print object.__str__(i)+' stars'
+        D=get_labeled_comments(i)
+        if cle in D:
+            print D[cle] / D["total of comments"]
+        else:
+            print 0    
+        print
+    
+  
+#test()    
+cle="pue"
+frequency_of(cle)
